a33bc7eee926149daaa79d5527e41dfe62c474fe
Benjamin <bpayanpsl@gmail.com>
2020-01-31 16:54:36 +0100

    Initial commit

---
+++ EasyQuery/Form1.Designer.vb
@@ -0,0 +1,122 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form remplace la méthode Dispose pour nettoyer la liste des composants.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requise par le Concepteur Windows Form
+    Private components As System.ComponentModel.IContainer
+
+    'REMARQUE : la procédure suivante est requise par le Concepteur Windows Form
+    'Elle peut être modifiée à l'aide du Concepteur Windows Form.  
+    'Ne la modifiez pas à l'aide de l'éditeur de code.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.ComboBoxDatabases = New System.Windows.Forms.ComboBox()
+        Me.ComboBoxTables = New System.Windows.Forms.ComboBox()
+        Me.ComboBoxAction = New System.Windows.Forms.ComboBox()
+        Me.CheckedListBoxChamps = New System.Windows.Forms.CheckedListBox()
+        Me.BackgroundWorker1 = New System.ComponentModel.BackgroundWorker()
+        Me.RichTextBoxVisu = New System.Windows.Forms.RichTextBox()
+        Me.DataGridViewResult = New System.Windows.Forms.DataGridView()
+        Me.ButtonOK = New System.Windows.Forms.Button()
+        CType(Me.DataGridViewResult, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'ComboBoxDatabases
+        '
+        Me.ComboBoxDatabases.FormattingEnabled = True
+        Me.ComboBoxDatabases.Location = New System.Drawing.Point(22, 58)
+        Me.ComboBoxDatabases.Name = "ComboBoxDatabases"
+        Me.ComboBoxDatabases.Size = New System.Drawing.Size(251, 21)
+        Me.ComboBoxDatabases.TabIndex = 0
+        '
+        'ComboBoxTables
+        '
+        Me.ComboBoxTables.FormattingEnabled = True
+        Me.ComboBoxTables.Location = New System.Drawing.Point(22, 86)
+        Me.ComboBoxTables.Name = "ComboBoxTables"
+        Me.ComboBoxTables.Size = New System.Drawing.Size(251, 21)
+        Me.ComboBoxTables.TabIndex = 1
+        '
+        'ComboBoxAction
+        '
+        Me.ComboBoxAction.FormattingEnabled = True
+        Me.ComboBoxAction.Items.AddRange(New Object() {"SELECT", "INSERT INTO", "UPDATE", "DELETE"})
+        Me.ComboBoxAction.Location = New System.Drawing.Point(22, 114)
+        Me.ComboBoxAction.Name = "ComboBoxAction"
+        Me.ComboBoxAction.Size = New System.Drawing.Size(121, 21)
+        Me.ComboBoxAction.TabIndex = 2
+        '
+        'CheckedListBoxChamps
+        '
+        Me.CheckedListBoxChamps.CheckOnClick = True
+        Me.CheckedListBoxChamps.FormattingEnabled = True
+        Me.CheckedListBoxChamps.Location = New System.Drawing.Point(22, 141)
+        Me.CheckedListBoxChamps.Name = "CheckedListBoxChamps"
+        Me.CheckedListBoxChamps.Size = New System.Drawing.Size(250, 169)
+        Me.CheckedListBoxChamps.TabIndex = 3
+        '
+        'RichTextBoxVisu
+        '
+        Me.RichTextBoxVisu.Location = New System.Drawing.Point(301, 58)
+        Me.RichTextBoxVisu.Name = "RichTextBoxVisu"
+        Me.RichTextBoxVisu.Size = New System.Drawing.Size(348, 96)
+        Me.RichTextBoxVisu.TabIndex = 4
+        Me.RichTextBoxVisu.Text = ""
+        '
+        'DataGridViewResult
+        '
+        Me.DataGridViewResult.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridViewResult.Location = New System.Drawing.Point(301, 160)
+        Me.DataGridViewResult.Name = "DataGridViewResult"
+        Me.DataGridViewResult.Size = New System.Drawing.Size(753, 264)
+        Me.DataGridViewResult.TabIndex = 5
+        '
+        'ButtonOK
+        '
+        Me.ButtonOK.Location = New System.Drawing.Point(107, 316)
+        Me.ButtonOK.Name = "ButtonOK"
+        Me.ButtonOK.Size = New System.Drawing.Size(75, 23)
+        Me.ButtonOK.TabIndex = 6
+        Me.ButtonOK.Text = "OK"
+        Me.ButtonOK.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1066, 454)
+        Me.Controls.Add(Me.ButtonOK)
+        Me.Controls.Add(Me.DataGridViewResult)
+        Me.Controls.Add(Me.RichTextBoxVisu)
+        Me.Controls.Add(Me.CheckedListBoxChamps)
+        Me.Controls.Add(Me.ComboBoxAction)
+        Me.Controls.Add(Me.ComboBoxTables)
+        Me.Controls.Add(Me.ComboBoxDatabases)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.DataGridViewResult, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents ComboBoxDatabases As System.Windows.Forms.ComboBox
+    Friend WithEvents ComboBoxTables As System.Windows.Forms.ComboBox
+    Friend WithEvents ComboBoxAction As System.Windows.Forms.ComboBox
+    Friend WithEvents CheckedListBoxChamps As System.Windows.Forms.CheckedListBox
+    Friend WithEvents BackgroundWorker1 As System.ComponentModel.BackgroundWorker
+    Friend WithEvents RichTextBoxVisu As System.Windows.Forms.RichTextBox
+    Friend WithEvents DataGridViewResult As System.Windows.Forms.DataGridView
+    Friend WithEvents ButtonOK As System.Windows.Forms.Button
+
+End Class
---
+++ EasyQuery/Form1.vb
@@ -0,0 +1,127 @@
+﻿Imports System.Data.SqlClient
+
+Public Class Form1
+
+    Dim connectionString As String = "Data Source=serveur_silog;Initial Catalog=master;User ID=sa;Password="
+    Dim con = New SqlConnection(connectionString)
+    Dim database As String
+    Dim table As String
+    Dim schema As String
+    Dim finalQuery As String
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        chargeDatabases()
+    End Sub
+
+    Private Sub chargeDatabases()
+        ComboBoxDatabases.Items.Clear()
+        con.open()
+        Dim command As New SqlCommand("SELECT name FROM sys.databases order by name", con)
+        Dim reader As SqlDataReader = command.ExecuteReader
+        If reader.HasRows Then
+            While reader.Read
+                ComboBoxDatabases.Items.Add(reader("name"))
+            End While
+        End If
+        reader.Close()
+        con.close()
+    End Sub
+
+    Private Sub ComboBoxDatabases_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ComboBoxDatabases.SelectedIndexChanged
+        database = ComboBoxDatabases.Text
+        connectionString = "Data Source=serveur_silog;Initial Catalog=" & database & ";User ID=sa;Password="
+        con = New SqlConnection(connectionString)
+        chargeTables()
+    End Sub
+
+    Private Sub chargeTables()
+        ComboBoxTables.Items.Clear()
+        con.open()
+        Dim command As New SqlCommand("SELECT table_name FROM information_schema.tables ORDER BY table_name", con)
+        Dim reader As SqlDataReader = command.ExecuteReader
+        If reader.HasRows Then
+            While reader.Read
+                ComboBoxTables.Items.Add(reader("table_name"))
+            End While
+        End If
+        reader.Close()
+        con.close()
+    End Sub
+
+    Private Sub ComboBoxAction_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ComboBoxAction.SelectedIndexChanged
+
+    End Sub
+
+    Private Sub chargeChamps()
+        CheckedListBoxChamps.Items.Clear()
+        CheckedListBoxChamps.Items.Add("*")
+        con.open()
+        Dim command As New SqlCommand("SELECT COLUMN_NAME FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = '" & table & "'", con)
+        Dim reader As SqlDataReader = command.ExecuteReader
+        If reader.HasRows Then
+            While reader.Read
+                CheckedListBoxChamps.Items.Add(reader("column_name"))
+            End While
+        End If
+        reader.Close()
+        con.Close()
+    End Sub
+
+    Private Sub ComboBoxTables_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ComboBoxTables.SelectedIndexChanged
+        con.open()
+        Dim query As String = "SELECT table_schema FROM information_schema.tables where table_name = '" & ComboBoxTables.Text & "'"
+        Console.WriteLine(query)
+        Dim command As New SqlCommand(query, con)
+        Dim reader As SqlDataReader = command.ExecuteReader
+        reader.Read()
+        If reader.HasRows Then
+            table = ComboBoxTables.Text
+            schema = reader("table_schema")
+        End If
+        reader.Close()
+        con.close()
+        chargeChamps()
+    End Sub
+
+    Private Sub CheckedListBoxChamps_ItemCheck(sender As Object, e As ItemCheckEventArgs) Handles CheckedListBoxChamps.ItemCheck
+        If Not (CheckedListBoxChamps.CheckedItems.Count = 1 And e.NewValue = CheckState.Unchecked) Then
+            finalQuery = ""
+            finalQuery = ComboBoxAction.Text & " "
+            Dim first As Boolean = True
+            For Each item In CheckedListBoxChamps.CheckedItems
+                If first Then
+                    finalQuery = finalQuery & item.ToString
+                    first = False
+                Else
+                    finalQuery = finalQuery & ", " & item.ToString
+                End If
+            Next
+            If e.NewValue = CheckState.Checked Then
+                If Not first Then
+                    finalQuery = finalQuery & ", " & CheckedListBoxChamps.Items(e.Index)
+                Else
+                    finalQuery = finalQuery & CheckedListBoxChamps.Items(e.Index)
+                End If
+            Else
+                finalQuery = Replace(finalQuery, CheckedListBoxChamps.Items(e.Index), "")
+            End If
+            finalQuery = finalQuery & " FROM " & schema & "." & table
+            RichTextBoxVisu.Clear()
+            RichTextBoxVisu.Text = finalQuery
+        Else
+            RichTextBoxVisu.Clear()
+            finalQuery = ""
+        End If
+    End Sub
+
+    Private Sub ButtonOK_Click(sender As Object, e As EventArgs) Handles ButtonOK.Click
+        Dim sqlCmd As New SqlCommand()
+        sqlCmd.Connection = con
+        sqlCmd.CommandType = CommandType.Text
+        sqlCmd.CommandText = finalQuery
+        Dim sqlDataAdap As New SqlDataAdapter(sqlCmd)
+        Dim dtRecord As New DataTable()
+        sqlDataAdap.Fill(dtRecord)
+        DataGridViewResult.DataSource = dtRecord
+    End Sub
+End Class
